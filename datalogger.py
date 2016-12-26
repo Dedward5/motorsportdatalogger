57ce67ba2a0e78e0a6a0e3e8d223acb44a518c11
Dedward5 <david@zennor.co.uk>
2016-12-26 16:45:46 +0000

    Added text Started text to matrix
    Added the code to show the word Started when the code begins to run.

--- datalogger.py
+++ datalogger.py
@@ -51,6 +51,8 @@ return sense_data
 
 sense = SenseHat()
 
+sense.show_message("Started", scroll_speed=0.05, text_colour=[255,255,0], back_colour=[0,0,255])
+
 while True:
   sense_data = get_sense_data()
   print(sense_data)
