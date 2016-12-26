b95a74a50c8fbef6f97061d8a8513aadc9f417ed
Dedward5 <david@zennor.co.uk>
2016-12-26 21:52:05 +0000

    Basic up and running (#1)
    * Update README.md
    
    * First attempt to get some data on the screen
    
    Should display some data on the screen, not the sense hat matrix, just the VDU.
    
    * Added sections
    
    Added sections and some more info
    
    * Added text Started text to matrix
    
    Added the code to show the word Started when the code begins to run.
    
    * Added dependency
    
    Added about installing Python
    
    * Removed incorrect tabs
    
    Some indents were illegal
    
    * Removed more errant indents
    
    * Update README.md
    
    * Fixed error with case on HAT
    
    Should be SenseHat not SenseHAT

---
+++ datalogger.py
@@ -0,0 +1,58 @@
+# Raceing car data logger for SenseHat equipped Raspberry Pi
+# By David Edwards
+
+
+#### Libraries #####
+from sense_hat import SenseHat
+from datetime import datetime
+
+
+#### Functions ####
+
+def get_sense_data():
+  sense_data=[]
+
+  sense_data.append(sense.get_temperature_from_humidity())
+  sense_data.append(sense.get_temperature_from_pressure())
+  sense_data.append(sense.get_humidity())
+  sense_data.append(sense.get_pressure())
+  
+  o = sense.get_orientation()
+  yaw = o["yaw"]
+  pitch = o["pitch"]
+  roll = o["roll"]
+  sense_data.extend([pitch,roll,yaw])
+
+  mag = sense.get_compass_raw()
+  mag_x = mag["x"]
+  mag_y = mag["y"]
+  mag_z = mag["z"]
+  sense_data.extend([mag_x,mag_y,mag_z])
+
+  acc = sense.get_accelerometer_raw()
+  x = acc["x"]
+  y = acc["y"]
+  z = acc["z"]
+  sense_data.extend([x,y,z])
+
+  gyro = sense.get_gyroscope_raw()
+  gyro_x = gyro["x"]
+  gyro_y = gyro["y"]
+  gyro_z = gyro["z"]
+  sense_data.extend([gyro_x,gyro_y,gyro_z])
+
+  sense_data.append(datetime.now())
+
+  return sense_data
+  
+  
+
+#### Main Program ####
+
+sense = SenseHat()
+
+sense.show_message("Started", scroll_speed=0.05, text_colour=[255,255,0], back_colour=[0,0,255])
+
+while True:
+  sense_data = get_sense_data()
+  print(sense_data)
