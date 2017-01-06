ad7a28c3473aa7450a920fc4e4e4f0f8be43090c
Dedward5 <david@zennor.co.uk>
2017-01-06 14:02:34 +0000

    Added sense data display code
    Should possibly now display sense data on keydown but im not sure how it will loop or stop.

--- datalogger.py
+++ datalogger.py
@@ -81,6 +81,8 @@ try:
         if event.type == ecodes.EV_KEY:
             if event.value == 1:  # key down
                 print ("keydown")
+                sense_data = get_sense_data ()
+                print (sense_data)
             if event.value == 0:  # key up
                 print ("key upppppppppppp")
 except KeyboardInterrupt:
