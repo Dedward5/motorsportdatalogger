421426c4d094915c720eeadc6268ac655833e1ed
Dedward5 <david@zennor.co.uk>
2017-01-07 21:52:19 +0000

    Show scrolling logging text
    Show the word logging on the matrix

--- datalogger.py
+++ datalogger.py
@@ -87,6 +87,7 @@ try:
               
               try:
                   for event in dev.read_loop():
+                      sense.show_message("Logging", scroll_speed=0.05, text_colour=[255,255,0], back_colour=[0,0,255]) # Show some text on matrix
                       if event.type == ecodes.EV_KEY:
                         if event.code == ecodes.KEY_ENTER and event.value == 1:
                             logging = Flase 
