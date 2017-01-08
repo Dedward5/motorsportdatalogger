9507413babe1600b16d6a15bab948fdd784515f0
Dedward5 <david@zennor.co.uk>
2017-01-08 00:16:01 +0000

    Update datalogger.py

--- datalogger.py
+++ datalogger.py
@@ -56,7 +56,8 @@ def run_log(): # funtion to pick up joystick input
          if event.type == ecodes.EV_KEY:
           if event.code == ecodes.KEY_ENTER and event.value == 1:
             value = (1, 0)[value]  
-            print (value)
+            while value
+              print ("wibble")
   except KeyboardInterrupt:
           sys.exit()
   return value 
