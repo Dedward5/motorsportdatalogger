ddeb739eda43a2d9d62ffb85405e36bb5981fe3f
David Edwards <David.edwards@digital.homeoffice.gov.uk>
2017-01-14 20:09:03 +0000

    Removed some test files
    Removed random files with attempts to read a joystick input.

--- basicjoystick.py
+++
@@ -1,6 +0,0 @@
-from sense_hat import SenseHat
-
-sense = SenseHat()
-while True:
-    for event in sense.stick.get_events():
-        print("The joystick was {} {}".format(event.action, event.direction))
