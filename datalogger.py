5ce7ea5179427587755e3fdffce0606296420948
Dedward5 <david@zennor.co.uk>
2017-01-15 20:37:16 +0000

    Added sense data into loop

--- datalogger.py
+++ datalogger.py
@@ -76,4 +76,6 @@ sense.stick.direction_middle = joystick_push
 while True:
   print("Waiting.....")
   while value:
-    print("logging")
+    print ("logging")
+    sense_data = get_sense_data()
+    print (sense_data)
