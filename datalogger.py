9426d6ef77e74dd0a972f33feec96429cb035dac
Dedward5 <david@zennor.co.uk>
2017-03-02 20:59:43 +0000

    re-ordered data output

--- datalogger.py
+++ datalogger.py
@@ -51,12 +51,12 @@ def log_data ():
   batch_data.append(output_string)
 
 def file_setup(filename):
-  header  =["temp_h","temp_p","humidity","pressure",
+  header  =["timestamp",
+  "accel_x","accel_y","accel_z",
   "pitch","roll","yaw",
   "mag_x","mag_y","mag_z",
-  "accel_x","accel_y","accel_z",
   "gyro_x","gyro_y","gyro_z",
-  "timestamp"]
+  "temp_h","temp_p","humidity","pressure"]
 
   with open(filename,"w") as f:
       f.write(",".join(str(value) for value in header)+ "\n")
@@ -64,11 +64,14 @@ def file_setup(filename):
 def get_sense_data(): # Main function to get all the sense data
   sense_data=[]
 
-  sense_data.append(sense.get_temperature_from_humidity())
-  sense_data.append(sense.get_temperature_from_pressure())
-  sense_data.append(sense.get_humidity())
-  sense_data.append(sense.get_pressure())
-  
+  sense_data.append(datetime.now())
+
+  acc = sense.get_accelerometer_raw()
+  x = acc["x"]
+  y = acc["y"]
+  z = acc["z"]
+  sense_data.extend([x,y,z])
+
   o = sense.get_orientation()
   yaw = o["yaw"]
   pitch = o["pitch"]
@@ -81,19 +84,18 @@ def get_sense_data(): # Main function to get all the sense data
   mag_z = mag["z"]
   sense_data.extend([mag_x,mag_y,mag_z])
 
-  acc = sense.get_accelerometer_raw()
-  x = acc["x"]
-  y = acc["y"]
-  z = acc["z"]
-  sense_data.extend([x,y,z])
-
   gyro = sense.get_gyroscope_raw()
   gyro_x = gyro["x"]
   gyro_y = gyro["y"]
   gyro_z = gyro["z"]
   sense_data.extend([gyro_x,gyro_y,gyro_z])
 
-  sense_data.append(datetime.now())
+  sense_data.append(sense.get_temperature_from_humidity())
+  sense_data.append(sense.get_temperature_from_pressure())
+  sense_data.append(sense.get_humidity())
+  sense_data.append(sense.get_pressure())
+
+
 
   return sense_data
 
